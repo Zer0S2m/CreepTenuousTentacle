d7e4f733718480708e3d68d6c00524d17c2474b2
Aleksandr <ZeroDay9898@gmail.com>
2023-07-01 21:18:28 +0300

    Init

---
+++ src/main/java/com/zer0s2m/creeptenuoustentacle/CreepTenuousTentacleApplication.java
@@ -0,0 +1,13 @@
+package com.zer0s2m.creeptenuoustentacle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreepTenuousTentacleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CreepTenuousTentacleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zer0s2m/creeptenuoustentacle/CreepTenuousTentacleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zer0s2m.creeptenuoustentacle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreepTenuousTentacleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
